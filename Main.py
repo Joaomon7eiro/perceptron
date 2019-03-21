db058919c048b7489c56b0c90177bdf1e81633b9
joao monteiro <joaoagrom@gmail.com>
2019-03-20 22:51:25 -0300

    ex 3 and 4 done

--- Main.py
+++ Main.py
@@ -114,7 +114,6 @@ def exercise_two():
         0, 0, 1, 0, 0,
     ]))
 
-
     print(perceptron.test([
         1, 1, 1, 1, 1,
         0, 0, 1, 0, 0,
@@ -138,8 +137,304 @@ def exercise_two():
         1, 1, 1, 1, 1,
         1, 0, 0, 0, 1,
     ]))
+
+
+def exercise_three():
+    #           a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+    targets1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    targets2 = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+    targets3 = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+    targets4 = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0]
+    targets5 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+    patterns = [
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 1, 1, 1, 0,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 0,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 0,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 1, 1, 1, 0,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 0,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 0, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            0, 0, 1, 0, 0,
+            1, 0, 1, 0, 0,
+            1, 0, 1, 0, 0,
+            1, 1, 1, 0, 0,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 0, 0, 1, 0,
+            1, 1, 1, 0, 0,
+            1, 0, 0, 1, 0,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 1, 0, 1, 1,
+            1, 0, 1, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 1, 0, 0, 1,
+            1, 0, 1, 0, 1,
+            1, 0, 0, 1, 1,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+        ],
+        [
+            0, 1, 1, 1, 0,
+            1, 0, 0, 0, 1,
+            1, 0, 1, 0, 1,
+            1, 0, 0, 1, 1,
+            0, 1, 1, 1, 1,
+        ],
+        [
+            1, 1, 1, 1, 0,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 0,
+            1, 0, 0, 1, 0,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0,
+            1, 1, 1, 1, 1,
+            0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0,
+            0, 1, 0, 1, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 1, 0, 1,
+            1, 1, 0, 1, 1,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            0, 1, 0, 1, 0,
+            0, 0, 1, 0, 0,
+            0, 1, 0, 1, 0,
+            1, 0, 0, 0, 1,
+        ],
+        [
+            1, 0, 0, 0, 1,
+            0, 1, 0, 1, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 1, 0, 0,
+        ],
+        [
+            1, 1, 1, 1, 1,
+            0, 0, 0, 1, 0,
+            0, 0, 1, 0, 0,
+            0, 1, 0, 0, 0,
+            1, 1, 1, 1, 1,
+        ]
+    ]
+
+    perceptron1 = Perceptron(26)
+    perceptron2 = Perceptron(26)
+    perceptron3 = Perceptron(26)
+    perceptron4 = Perceptron(26)
+    perceptron5 = Perceptron(26)
+
+    learning_rate = 0.2
+
+    perceptron1.train(patterns, targets1, learning_rate, 1000)
+    perceptron2.train(patterns, targets2, learning_rate, 1000)
+    perceptron3.train(patterns, targets3, learning_rate, 1000)
+    perceptron4.train(patterns, targets4, learning_rate, 1000)
+    perceptron5.train(patterns, targets5, learning_rate, 1000)
+
+    print(perceptron1.test([
+        1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+    ]))
+    print(perceptron2.test([
+        1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+    ]))
+    print(perceptron3.test([
+        1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+    ]))
+    print(perceptron4.test([
+        1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+    ]))
+    print(perceptron5.test([
+        1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+    ]))
+
+
+def exercise_four():
+    perceptron_nand = Perceptron(2)
+    perceptron_or = Perceptron(2)
+
+    target_nand = [1, 1, 1, 0]
+    target_or = [0, 1, 1, 1]
+
+    patterns = [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1]
+    ]
+
+    learning_rate = 0.2
+
+    perceptron_and = Perceptron(2)
+    target_and = [0, 0, 0, 1]
+
+    perceptron_nand.train(patterns, target_nand, learning_rate, 1000)
+    perceptron_and.train(patterns, target_and, learning_rate, 1000)
+    perceptron_or.train(patterns, target_or, learning_rate, 1000)
+
+    and_inputs = []
+
+    and_inputs.append(perceptron_nand.test([0, 0]))
+    and_inputs.append(perceptron_or.test([0, 0]))
+    print(perceptron_and.test(and_inputs))
+
+
+    and_inputs = []
+
+    and_inputs.append(perceptron_nand.test([0, 1]))
+    and_inputs.append(perceptron_or.test([0, 1]))
+    print(perceptron_and.test(and_inputs))
+
+
+    and_inputs = []
+
+    and_inputs.append(perceptron_nand.test([1, 0]))
+    and_inputs.append(perceptron_or.test([1, 0]))
+    print(perceptron_and.test(and_inputs))
+
+    and_inputs = []
+
+    and_inputs.append(perceptron_nand.test([1, 1]))
+    and_inputs.append(perceptron_or.test([1, 1]))
+    print(perceptron_and.test(and_inputs))
 
 
 if __name__ == "__main__":
-    #exercise_one()
-    exercise_two()
+# exercise_one()
+# exercise_two()
+# exercise_three()
+    exercise_four()
